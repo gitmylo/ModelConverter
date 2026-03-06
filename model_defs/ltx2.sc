d6ef99ebc87d3c8923c9f598925308d99c57dbd3
Mylo <36931363+gitmylo@users.noreply.github.com>
2026-03-06 08:54:09 +0100

    Untested ltx 2 and umt5 convert configs

---
+++ model_defs/ltx2.sc
@@ -0,0 +1,6 @@
+﻿e".weight" "model.diffusion_model.transformer_blocks"
+ "attn"
+  "to_gate" = x
+  "gate" = 0
+ "ff.net" = 0
+x
--- print_weight_names.py
+++ print_weight_names.py
@@ -4,7 +4,9 @@ import safetensors.torch
 
 file = sys.argv[1]
 model = safetensors.torch.load_file(file)
-qmeta = safetensors.safe_open(file, "pt").metadata().get("_quantization_metadata", None)
+qmeta = safetensors.safe_open(file, "pt").metadata()
+if qmeta is not None:
+    qmeta = qmeta.get("_quantization_metadata", None)
 
 print(", ".join(model.keys()))
 print(qmeta)
